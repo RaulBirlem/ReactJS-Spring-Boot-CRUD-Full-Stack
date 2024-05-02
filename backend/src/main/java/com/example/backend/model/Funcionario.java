9d33c33e98ad1fcb14963ea43f86d689de434be7
RaulBirlem <raul.birlem@gmail.com>
2024-05-02 16:05:13 -0300

    Funcionario getter and setter

--- backend/src/main/java/com/example/backend/model/Funcionario.java
+++ backend/src/main/java/com/example/backend/model/Funcionario.java
@@ -1,8 +1,64 @@
 package com.example.backend.model;
 
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "funcionarios")
 public class Funcionario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Column(name = "first_name")
     private String firstName;
+    @Column(name = "last_name")
     private String lastName;
-    private String emailId
+    @Column(name = "email_id")
+    private String emailId;
+
+    public Funcionario() {
+
+    }
+
+    public Funcionario(String emailId, String firstName, String lastName) {
+        this.emailId = emailId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+
 }
