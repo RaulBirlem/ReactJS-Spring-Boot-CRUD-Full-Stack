8d1d8ed6009f6749292d16424b071ed8843b0508
RaulBirlem <raul.birlem@gmail.com>
2024-05-25 15:01:17 -0300

    add Employee frontend

--- frontend/src/components/EmployeeComponent.jsx
+++ frontend/src/components/EmployeeComponent.jsx
@@ -1,8 +1,67 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const EmployeeComponent = () => {
+
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
+    const [email, setEmail] = useState('')
+
+    
+    function saveEmployee(e) {
+        e.preventDefault();
+        const employee = {firstName, lastName, email}
+        console.log(employee)
+    }
+
   return (
-    <div>EmployeeComponent</div>
+    <div className='container'>
+        <br /> <br />
+        <div className="row">
+            <div className="card col-md-6 offset-md-3 offset-md-3">
+                <h2 className='text-center'>Adicionar Funcionário</h2>
+                <div className='card-body'>
+                    <form>
+                        <div className="form-group mb-2">
+                            <label className='form-label'>Nome:</label>
+                            <input type="text" 
+                            placeholder='Digite seu nome' 
+                            name='firstName' 
+                            value={firstName} 
+                            className='form-control'
+                            onChange={(e) =>setFirstName(e.target.value)}
+                            />
+                        </div>
+
+                        <div className="form-group mb-2">
+                            <label className='form-label'>Sobrenome:</label>
+                            <input type="text" 
+                            placeholder='Digite seu sobrenome' 
+                            name='lastName' 
+                            value={lastName} 
+                            className='form-control'
+                            onChange={(e) =>setLastName(e.target.value)}
+                            />
+                        </div>
+
+                        <div className="form-group mb-2">
+                            <label className='form-label'>Email:</label>
+                            <input type="text" 
+                            placeholder='Digite seu email' 
+                            name='email' 
+                            value={email} 
+                            className='form-control'
+                            onChange={(e) =>setEmail(e.target.value)}
+                            />
+                        </div>
+
+
+                        <button className="btn btn-success" onClick={saveEmployee}>Ok</button>
+                    </form>
+
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
