fcf4fcb258f64963a934c240f5145e5f711490bb
RaulBirlem <raul.birlem@gmail.com>
2024-05-25 13:44:41 -0300

    new frontend

--- backend/src/main/java/net/javaguides/cms/entity/Employee.java
+++ backend/src/main/java/net/javaguides/cms/entity/Employee.java
@@ -17,7 +17,7 @@ public class Employee {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "firts_name")
+    @Column(name = "first_name")
     private String firstName;
     @Column(name = "last_name")
     private String lastName;
