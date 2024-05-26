e76df703bc47c0566f9f3e7f2c1a010c222ef6da
RaulBirlem <raul.birlem@gmail.com>
2024-05-26 17:13:47 -0300

    project completed

--- frontend/src/components/ListEmployeeComponent.jsx
+++ frontend/src/components/ListEmployeeComponent.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react'
-import { listEmployees } from '../services/EmployeeService'
+import { deleteEmployee, listEmployees } from '../services/EmployeeService'
 import {useNavigate} from 'react-router-dom'
 
 const ListEmployeeComponent = () => {
@@ -9,14 +9,17 @@ const ListEmployeeComponent = () => {
     const navigator = useNavigate();
 
     useEffect(() =>{
+       getAllEmployees();
+    },[])
+
+    function getAllEmployees() {
         listEmployees().then((response) =>{
             setEmployees(response.data);
 
         }).catch(error => {
-            console.log(error);
+            console.error(error);
         })
-    },[])
-
+    }
 
 
     function addNewEmployee() {
@@ -25,6 +28,16 @@ const ListEmployeeComponent = () => {
 
     function updateEmployee(id) {
         navigator(`/edit-employee/${id}`)
+    }
+
+    function removeEmployee(id) {
+        console.log(id)
+
+        deleteEmployee(id).then((response) => {
+            getAllEmployees();
+        }).catch(error => {
+            console.error(error)
+        })
     }
 
   return (
@@ -54,6 +67,9 @@ const ListEmployeeComponent = () => {
                             <td>{employee.email}</td>
                             <td>
                                 <button className='btn btn-info' onClick={() => updateEmployee(employee.id)}>Alterar</button>
+                                <button className='btn btn-danger' 
+                                onClick={() => removeEmployee(employee.id)}
+                                style={{marginLeft:'10px'}}>Delete</button>
                             </td>
 
                         </tr>
--- frontend/src/services/EmployeeService.js
+++ frontend/src/services/EmployeeService.js
@@ -13,3 +13,4 @@ export const getEmployee = (employeeId) => axios.get(REST_API_BASE_URL + '/' + e
 
 export const updateEmployee = (employeeId, employee) => axios.put(REST_API_BASE_URL + '/' + employeeId, employee );
 
+export const deleteEmployee = (employeeId) => axios.delete(REST_API_BASE_URL + '/' + employeeId );
